429ec30d3b4df6a2162b4a689a528562626e0011
shakibbash <shaking235@gmail.com>
2025-01-31 11:19:24 +0600

    trending pets section done it

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,3 +6,24 @@ module.exports = {
     },
     plugins: [],
   }
+//   <div class="bg-white rounded-lg p-4 flex items-center gap-4 w-64">
+//   <!-- Image -->
+//   <div class="bg-orange-100 p-2 rounded-lg">
+//       <img src="c1-assets/fish-cartoon.png" alt="Fish Icon" class="w-12 h-12">
+//   </div>
+
+//   <!-- Content -->
+//   <div>
+//       <!-- Rating -->
+//       <div class="flex items-center space-x-1">
+//           <span class="text-yellow-500 text-lg">⭐</span>
+//           <span class="text-gray-700 text-sm">4.5</span>
+//       </div>
+
+//       <!-- Title -->
+//       <h2 class="text-lg font-bold">Pet Bed</h2>
+
+//       <!-- Price -->
+//       <p class="text-gray-600 font-semibold">$39.99</p>
+//   </div>
+// </div>
